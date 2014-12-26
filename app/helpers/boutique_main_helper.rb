cd400d245522579a4c0caff35905f26e9ca9c6eb
kachon <kachon10@yahoo.com>
2014-12-26 15:55:18 -0800

    test logger

--- app/helpers/boutique_main_helper.rb
+++ app/helpers/boutique_main_helper.rb
@@ -1,7 +1,8 @@
 module BoutiqueMainHelper
   
   def check_user
-    puts "check user"
+    Rails.logger.info "check user through logger"
+    puts "check user through puts"
     # need to enter code here to return error if user is not found
     # render :json => {
     #   :abc => 111,
--- config/initializers/object_id.rb
+++ config/initializers/object_id.rb
@@ -1,7 +1,5 @@
 module BSON
   class ObjectId
-    Rails.logger.info "Object Id??"
-    puts "puts Object Id"
     def as_json(options = {})
       to_s
     end
